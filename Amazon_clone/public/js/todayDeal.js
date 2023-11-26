f53ae14da01a917109e7d82cbf6a6391fde819ae
Sukanta5757 <sukantanayak5757@gmail.com>
2023-11-26 11:00:01 +0530

    created public dir . then html, css, js file inside the public dir .

--- Amazon_clone/public/js/todayDeal.js
+++ Amazon_clone/public/js/todayDeal.js
@@ -1,112 +1,112 @@
 export const todayDeal = [
     {
-        img : "images/box1_image.jpg",
+        img : "/Amazon_clone/images/box1_image.jpg",
         discount : 32,
         DealOfDay : "Deal of the Day",
         desc : " Grand Gaming Days offers on Bestselling Gaming Laptops & Desktops - Exchange & No Cost EMI Available"
     },
     {
-        img : "images/box2_image.jpg",
+        img : "/Amazon_clone/images/box2_image.jpg",
         discount : 31,
         DealOfDay : "Deal of the Day",
         desc : "High Performance Handpicked Desktop Computers from HP, Lenovo and more"
     },
     {
-        img : "images/box3_image.jpg",
+        img : "/Amazon_clone/images/box3_image.jpg",
         discount : 66,
         DealOfDay : "Deal of the Day",
         desc : "Best Prices on boAt Headphones, Soundbars and Speakers"
     },
     {
-        img : "images/box4_image.jpg",
+        img : "/Amazon_clone/images/box4_image.jpg",
         discount : 80,
         DealOfDay : "Deal of the Day",
         desc : "Jaw dropping deals on headsets"
     },
 
     {
-        img : "images/box5_image.jpg",
+        img : "/Amazon_clone/images/box5_image.jpg",
         discount : 50,
         DealOfDay : "Deal of the Day",
         desc : "adidas & campus Footwear"
     },
     {
-        img : "images/box6_image.jpg",
+        img : "/Amazon_clone/images/box6_image.jpg",
         discount : 64,
         DealOfDay : "Deal of the Day",
         desc : "Grand Gaming Days Offers on Accessories and Storage Devices"
     },
 
     {
-        img : "images/box7_image.jpg",
+        img : "/Amazon_clone/images/box7_image.jpg",
         discount : 54,
         DealOfDay : "Deal of the Day",
         desc : "cooking essential"
     },
     {
-        img : "images/box8_image.jpg",
+        img : "/Amazon_clone/images/box8_image.jpg",
         discount : 52,
         DealOfDay : "Deal of the Day",
         desc : "TOP Deals on PUMA & Skechers footwear"
     },
 
     {
-        img : "images/box7_image.jpg",
+        img : "/Amazon_clone/images/box7_image.jpg",
         discount : 12,
         DealOfDay : "Deal of the Day",
         desc : "Handpicked Intel Powered Laptops; High Performance"
     },
     {
-        img : "images/box8_image.jpg",
+        img : "/Amazon_clone/images/box8_image.jpg",
         discount : 65,
         DealOfDay : "Deal of the Day",
         desc : "Powerbank from Mi, Ambrane, URBN and more"
     },
 
     {
-        img : "images/box2_image.jpg",
+        img : "/Amazon_clone/images/box2_image.jpg",
         discount : 65,
         DealOfDay : "Deal of the Day",
         desc : "Top deals on Truke, Govo and more"
     },
     {
-        img : "images/box7_image.jpg",
+        img : "/Amazon_clone/images/box7_image.jpg",
         discount : 75,
         DealOfDay : "Deal of the Day",
         desc : "Amazing deals on pTron, Jabra, Portronics"
     },
     {
-        img : "images/box6_image.jpg",
+        img : "/Amazon_clone/images/box6_image.jpg",
         discount : 82,
         DealOfDay : "Deal of the Day",
         desc : "Levi's, Allen Solly, ANNI DESIGNER, Janasya & more"
     },
     {
-        img : "images/box5_image.jpg",
+        img : "/Amazon_clone/images/box5_image.jpg",
         discount : 77,
         DealOfDay : "Deal of the Day",
         desc : "Exciting  deals on Mivi, Wecool, Wings"
     },
     {
-        img : "images/box4_image.jpg",
+        img : "/Amazon_clone/images/box4_image.jpg",
         discount : 20,
         DealOfDay : "Deal of the Day",
         desc : "Never before deals on HP printers"
     },
     {
-        img : "images/box3_image.jpg",
+        img : "/Amazon_clone/images/box3_image.jpg",
         discount : 19,
         DealOfDay : "Deal of the Day",
         desc : "Vivo Y75 - 18W fast charge, FHD plus display"
     },
     {
-        img : "images/box2_image.jpg",
+        img : "/Amazon_clone/images/box2_image.jpg",
         discount : "25",
         DealOfDay : "Deal of the Day",
         desc : "Nokia 5710 - newly launched 4G feature phone"
     },
     {
-        img : "images/box1_image.jpg",
+        img : "/Amazon_clone/images/box1_image.jpg",
         discount : 32,
         DealOfDay : "Deal of the Day",
         desc : "Vacuum Cleaners from Top Brands"
